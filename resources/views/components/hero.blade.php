d5b341a5681714b730cc58e47f80fc1cd94a5206
Fikri Nurhakim <108182945+akufikri@users.noreply.github.com>
2024-01-31 19:19:28 +0700

    Create Hero Sections

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,69 @@
+<section class=" dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1
+                class="max-w-2xl text-blue-700 mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+                Kursus seru dan menarik di <span
+                    class="inline-block border-4 font-extrabold bg-cyan-100 max-w-xs border-blue-700 text-4xl px-5 py-1 rounded-2xl transform -rotate-2 shadow-2xl shadow-amber-200"><span
+                        class="text-[#FEC434]">B</span>elajar<span class="text-[#FEC434]">A</span>ja.id</span>
+            </h1>
+
+            <p class="max-w-2xl mt-5 text-blue-900 mb-6 font-light  lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                Pelajari beragam kursus berkualitas dengan pengajar ahli. Teori dan praktek langsung untuk meningkatkan
+                keterampilan Anda.
+            </p>
+
+            <a href="#"
+                class="inline-flex hover:bg-blue-600 shadow transition hover:-rotate-2 bg-blue-500 items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 focus:ring-4 ">
+                Get started
+                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"></path>
+                </svg>
+            </a>
+            <a href="#"
+                class="inline-flex items-center shadow transition  hover:rotate-2 text-gray-600 justify-center px-5 py-3 text-base font-medium text-center border-2 border-blue-300 rounded-lg  focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                <i class="fa-light fa-circle-play me-3"></i> <span>Learn More</span>
+            </a>
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="{{ asset('assets/image/model-1.png') }}" alt="mockup">
+        </div>
+    </div>
+</section>
+<div class="flex justify-center">
+    <div
+        class="h-36 mx-5 rounded-2xl p-4 max-w-6xl w-full absolute top-96 mt-64 backdrop-filter backdrop-blur-md bg-opacity-25 bg-blue-300">
+        <div class="flex gap-7 justify-between w-full px-9">
+            <div class="flex gap-4">
+                <div class="bg-blue-600 rounded-2xl p-5">
+                    <i class="fa-solid text-6xl text-[#FEC434] fa-book-open"></i>
+                </div>
+                <div class="grid">
+                    <h1 class="text-6xl font-bold text-blue-500">0+</h1>
+                    <span class="font-medium text-blue-800">Total Courses</span>
+                </div>
+            </div>
+            <div class="flex gap-4">
+                <div class="bg-blue-600 rounded-2xl px-7 pt-5">
+                    <i class="fa-solid text-6xl text-[#FEC434] fa-user"></i>
+                </div>
+                <div class="grid">
+                    <h1 class="text-6xl font-bold text-blue-500">0+</h1>
+                    <span class="font-medium text-blue-800">Total Mentors</span>
+                </div>
+            </div>
+            <div class="flex gap-4">
+                <div class="bg-blue-600 rounded-2xl p-5">
+                    <i class="fa-solid text-6xl text-[#FEC434] fa-users"></i>
+                </div>
+                <div class="grid">
+                    <h1 class="text-6xl font-bold text-blue-500">0+</h1>
+                    <span class="font-medium text-blue-800">Total Users</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
